d178291d57689f8ae7dfbd9b918e6c83fd8b271b
john-farina <clichecontent@gmail.com>
2023-02-16 19:53:15 -0500

    added importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-alert("hello");
+import React from "react";
+import * as ReactDOM from "react-dom";
+
+alert("cool");
+
+// function Test() {
+//   return <h1>hello</h1>;
+// }
+
+// ReactDOM.render(<Test />, document.querySelector("#app"));
